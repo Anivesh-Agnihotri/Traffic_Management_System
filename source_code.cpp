baadba916470e4883f019340e52f957a0fec21e5
Anivesh Agnihotri <54951713+Anivesh-Agnihotri@users.noreply.github.com>
2023-02-10 14:38:08 +0530

    Create source_code.cpp

---
+++ source_code.cpp
@@ -0,0 +1,11 @@
+//Smart Traffic Management System
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <fstream>
+#include <time.h>
+#include <iomanip>
+#include <cstdlib>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
