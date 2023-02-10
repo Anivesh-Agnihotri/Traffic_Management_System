7427e3ccad4cbaf1bcff565a73e7c9c47fbc4719
Anivesh Agnihotri <54951713+Anivesh-Agnihotri@users.noreply.github.com>
2023-02-10 14:45:53 +0530

    Update source_code.cpp

--- source_code.cpp
+++ source_code.cpp
@@ -198,3 +198,257 @@ void vehData::delete_data(char *t) {
 	getch();
 	f.record_veh();
 }
+void vehData::view_data() {
+	system("cls");
+	onlyDisplay d;
+	
+	gotoxy(65, 1); cout << "\xB2\xB2 Record of Challans Done \xB2\xB2\n\n";
+	
+	header();
+	ifstream fin;
+	fin.open("file1.dat", ios::in|ios::binary);
+	if(!fin)
+		cout << "\nFile Not Found !";
+	else {
+		fin.read((char*)this, sizeof(*this));
+		while(!fin.eof()) {
+			show_data();
+			fin.read((char*)this, sizeof(*this));	
+		}
+		fin.close();
+	}
+	
+	cout << "\n\n\n\nPress any key to continue...";
+	getch();
+	d.menu();    
+}
+
+void vehData::search_data(char *t) {
+	onlyDisplay d;
+	gotoxy(67, 1); cout << "\xB2\xB2 Search Record \xB2\xB2\n\n";
+	header();
+	int counter=0;
+	ifstream fin;
+	fin.open("file1.dat", ios::in|ios::binary);
+	if(!fin)
+		cout << "\nFile NOT Found !";
+	else {
+		fin.read((char*)this, sizeof(*this));
+		while(!fin.eof()) {
+			if(!strcmp(t, veh_no)) {
+				show_data();
+				counter++;
+			}
+			fin.read((char*)this, sizeof(*this));
+		}
+		if(counter==0)
+			cout << "\nRecord NOT Found";
+		fin.close();
+	}
+	cout << "\n\nPress any key to continue...";
+    getch();
+	d.menu();
+}
+
+void vehData::update_data(char *t) {
+	features f;
+	int found=0;
+	fstream file;
+	file.open("file1.dat", ios::in|ios::out|ios::ate|ios::binary);
+	file.seekg(0);
+	file.read((char*)this, sizeof(*this));
+	while(!file.eof()) {
+		if(!strcmp(t, veh_no)){
+			read_data();
+			int pos = (int)file.tellg()-sizeof(*this);
+			file.seekp(pos, ios::beg);
+			file.write((char*)this, sizeof(*this));
+			found++;
+		}
+		file.read((char*)this, sizeof(*this));
+	}
+	file.close();
+	if(found==0)
+		cout << "\n\tUnable to Update";
+	else
+		cout << "\n\tUpdated Successfully";
+	cout << "\n\tPress any key to continue...";
+	getch();
+	f.record_veh();
+}
+
+/*Booths Available*/
+void features::booths() {
+	system("cls");system("color 6");
+	onlyDisplay d;
+	gotoxy(65, 3);
+	cout << "\xB2\xB2\xB2\xB2  Traffic Control Booth  \xB2\xB2\xB2\xB2\n\n\n"; 
+	char ch;
+	ifstream fin;
+	fin.open("control_booth.dat");
+	while(!fin.eof()) {
+		ch = fin.get();
+		if(ch == '*') cout << "\xB2";
+		else cout << ch;
+	}
+	fin.close();
+	cout << "\n\n\n\t\tPress any key to continue...";
+	getch();
+	d.menu();
+}
+
+/*Helpline or Hospitality*/
+void features::helpline() {
+	system("cls");
+	onlyDisplay d;
+	gotoxy(65, 3);
+	cout << "\xB2\xB2\xB2\xB2  Helpline and Hospital Services  \xB2\xB2\xB2\xB2\n\n\n"; 
+	char ch;
+	ifstream fin;
+	fin.open("helpline.dat");
+	while(!fin.eof()) {
+		ch = fin.get();
+		if(ch == '*') cout << "\xB2";
+		else cout << ch;
+	}
+	fin.close();
+	cout << "\n\n\n\t\tPress any key to continue...";
+	getch();
+	d.menu();
+}
+
+/*Controls of Traffic*/
+void features::control() {
+	system("cls");
+	onlyDisplay d;
+	gotoxy(65, 3);
+	cout << "\xB2\xB2\xB2\xB2  Control of Traffic  \xB2\xB2\xB2\xB2\n\n\n"; 
+	char ch;
+	ifstream fin;
+	fin.open("control.dat");
+	while(!fin.eof()) {
+		ch = fin.get();
+		if(ch == '*') cout << "\xB2";
+		else cout << ch;
+	}
+	fin.close();
+	cout << "\n\n\n\t\tPress any key to continue...";
+	getch();
+	d.menu();
+}
+
+/*AUTHENTICATION*/
+void onlyDisplay::authentication() {
+	features f;system("color 0A");
+	int pkey;
+	system("cls");
+	gotoxy(65, 10);
+	for(i=0; i<25; i++)
+		cout << "\xD2"; 
+	cout << endl;	gotoxy(70, 12);
+	cout << "Authentication";	gotoxy(65, 14);
+	for(i=0; i<25; i++)
+		cout << "\xD2";
+		
+	gotoxy(55, 20);
+	cout << "\xB2\xB2 Enter the PIN : ";
+	static int i=3;
+	cin >> pkey;
+	if(pkey == pass) {
+		system("color 0A");
+		load();	
+		menu();
+	}else {
+		system("color 4");
+		gotoxy(50, 25);
+		cout << "Sorry, you are not authorised...";
+		cout << "Attempts left " << i;i--; 
+		getch();
+		if(i==-1) exit(0);
+		authentication();
+	}
+}
+
+/*LOADING SCREEN*/
+void onlyDisplay::load() {
+	system("cls");
+    int row,col,r,c,q;
+    gotoxy(81,19);
+    cout << "Loading...";
+    gotoxy(70,20);
+    for(r=1; r<=30; r++) {
+        for(q=0; q<=90000000; q++); //to display the character slowly
+    		printf("%c", 177);	
+    }
+}
+
+/*MENU SCREEN*/
+void onlyDisplay::menu() {
+	system("cls");
+	char srch[15];
+	features f; vehData vh;
+	system("color 9");
+	gotoxy(65, 5);
+	for(i=0; i<40; i++)
+		cout << "\xB2"; 
+	cout << endl;	gotoxy(70, 7);
+	cout << "Smart Traffic Management System";	gotoxy(65, 9);
+	for(i=0; i<40; i++)
+		cout << "\xB2"; 
+	cout << endl;	gotoxy(50, 13);
+	cout << "1. Keep the record of vehicles\n";	gotoxy(50, 14); 
+	cout << "2. Keep the record of challan done\n";	gotoxy(50, 15);
+	cout << "3. Search the record of vehicles\n";	gotoxy(50, 16);
+	cout << "4. Display information of traffic control booths\n";	gotoxy(50, 17);
+	cout << "5. Helpline information and nearby hospitals\n";	gotoxy(50, 18);
+	cout << "6. Control the traffic\n";	gotoxy(50, 19);
+	cout << "7. Exit";	gotoxy(50, 20);
+	
+	/*if(sound==0){
+		PlaySound("start.wav", NULL, SND_SYNC);
+		sound++;
+	}*/
+	
+	while(choice!=7) {
+		gotoxy(50, 22);
+		cout << "Enter Your Choice : ";
+		cin >> choice;
+		switch(choice) {
+			case 1:	f.record_veh(); break;
+			case 2:	vh.view_data(); break;
+			case 3:	{
+				system("cls");
+				gotoxy(50, 22);
+				cout << "Enter the Vehicle No to search : ";
+				cin.ignore();
+    			cin.getline(srch, 14);
+    			system("cls");
+				vh.search_data(srch);
+				break;
+			}
+			case 4:	f.booths(); break;
+			case 5:	f.helpline(); break;
+			case 6:	f.control(); break;
+			case 7: {
+				gotoxy(45, 30);
+				cout << "Thanks for using our Service, hope this helps you in your daily life";
+				getch();exit(0);break;
+			}
+		} 
+	}
+}
+
+/*DATE and TIME*/
+void date_time() {
+	time_t t = time(NULL);
+  	struct tm tm = *localtime(&t);
+  	dd = tm.tm_mday; mm = tm.tm_mon + 1; yy = tm.tm_year + 1900-2000;
+  	hr = tm.tm_hour; mint = tm.tm_min;
+}
+
+/*MOVING CURSOR POSITION*/
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x; coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
